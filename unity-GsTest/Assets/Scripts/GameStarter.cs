9a42704ab4002b1e8566062f9493cb69ca90c254
soraviv <soravit.v@varisoft.com>
2020-05-03 16:31:51 +0700

    Playfab login

---
+++ unity-GsTest/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab.ClientModels;
+using PlayFab;
+
+public class GameStarter : MonoBehaviour
+{
+    public PlayfabLogin playfabLogin;
+    void Start()
+    {
+        playfabLogin.gameObject.SetActive(true);
+        playfabLogin.InitializeCallback(OnLoginSuccess, OnLoginFail);
+    }
+    public void OnLoginSuccess(LoginResult loginResult)
+    {
+        //start game 
+        playfabLogin.gameObject.SetActive(false);
+    }
+    public void OnLoginFail(PlayFabError onLoginFail)
+    {
+
+    }
+}
---
+++ unity-GsTest/Assets/Scripts/PlayfabLogin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayfabLogin : MonoBehaviour
+{
+    public TMP_InputField customIdInputField;
+    public Button loginButton;
+    private System.Action<LoginResult> onLoginSuccess;
+    private System.Action<PlayFabError> onLoginFail;
+    private void Start()
+    {
+        loginButton.onClick.AddListener(Login);
+    }
+    public void Login()
+    {
+        loginButton.interactable = false;
+        var request = new LoginWithCustomIDRequest { CustomId = customIdInputField.text, CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, onLoginSuccess, onLoginFail);
+    }
+    public void InitializeCallback(System.Action<LoginResult> onLoginSuccess, System.Action<PlayFabError> onLoginFail)
+    {
+        this.onLoginSuccess = onLoginSuccess;
+        this.onLoginSuccess += OnLoginSuccessLog;
+        this.onLoginFail = onLoginFail;
+        this.onLoginFail += OnLoginFailureLog;
+    }
+    private void OnLoginSuccessLog(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        loginButton.interactable = false;
+    }
+
+    private void OnLoginFailureLog(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+        loginButton.interactable = false;
+    }
+}
