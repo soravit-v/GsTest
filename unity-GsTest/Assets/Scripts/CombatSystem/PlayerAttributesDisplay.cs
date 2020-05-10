2ff5e6042caef6fc9f03e58fa80aeb71618bf4ff
soraviv <soravit.v@varisoft.com>
2020-05-10 22:09:08 +0700

    Import some model and prep for animation

---
+++ unity-GsTest/Assets/Scripts/CombatSystem/PlayerAttributesDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class PlayerAttributesDisplay : MonoBehaviour
+{
+    public PlayerAttributes playerAttributes;
+    public RectTransform rectTransform;
+    public TMP_Text nameTextMesh;
+    public Image hpFill;
+    public Image mpFill;
+    public Image staminaFill;
+    
+    private void Awake()
+    {
+    }
+    public void SetName(string name)
+    {
+        nameTextMesh.text = name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerAttributes == null)
+            return;
+        hpFill.fillAmount = playerAttributes.HpRatio;
+        mpFill.fillAmount = playerAttributes.MpRatio;
+        staminaFill.fillAmount = playerAttributes.StaminaRatio;
+        FollowCharacter();
+    }
+    void FollowCharacter()
+    {
+        /*var screenPoint = Camera.main.WorldToScreenPoint(playerAttributes.transform.position);
+        screenPoint.z = 0;
+        rectTransform.position = screenPoint;*/
+        //transform.position = playerAttributes.transform.position;
+        transform.LookAt(Camera.main.transform);
+    }
+}
---
+++ unity-GsTest/Assets/Scripts/CombatSystem/PlayerAttributesPanel.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributesPanel : MonoBehaviour
+{
+    public Transform parent;
+    PlayerAttributesDisplay prefab;
+    public void AddPlayerAttribute(PlayerAttributes playerAttributes, Player player)
+    {
+        if (prefab == null)
+            prefab = Resources.Load<PlayerAttributesDisplay>("PlayerAttributesDisplay");
+        var display = Instantiate(prefab, parent);
+        display.SetName(player.UserId);
+        display.playerAttributes = playerAttributes;
+    }
+}
